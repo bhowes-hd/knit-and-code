378f4ca7c6c566f02029af44830d4625d1af166c
Benjamin Howes <82284712+bhowes-hd@users.noreply.github.com>
2023-05-07 17:26:30 -0400

    links and pratt icon

--- src/App.jsx
+++ src/App.jsx
@@ -54,9 +54,21 @@ function App() {
         <button onClick={() => glb.generateGlb()}>generate new 3d model</button>
       </div>
       <br></br>
-      <p className="read-the-docs">
+      <p>
         Use the buttons above to generate new 3d models, and to view a model in
         your space on AR-enabled devices.
+      </p>
+      <br></br>
+      <p className="read-the-docs">
+        Knit + Code is a mixed reality installation blending digital and
+        physical experiences into an immersive spectacle. At its core is a
+        minimal 3D-knit textile dynamically lit with a range of slowly changing
+        lights. This physical form creates a canvas for a digital mixed reality
+        experience: Viewers scan a QR code below the sculpture to launch into
+        the digital space, where a custom algorithm generates multi-colored
+        fibrous models that extend beyond the confines of the physical
+        structure. Knit + Code blurs the boundaries of digital fabrication and
+        spatial design, inviting viewers to explore the space in new ways.
       </p>
       <br></br>
       <p className="read-the-docs">
@@ -77,19 +89,19 @@ function App() {
       <br></br>
       <h3>Design Team</h3>
       <div>
-        <p className="team-member">
+        <p>
           <a href="https://www.hbh-design.com/" target="_blank">
             Hannah Berkin-Harper
           </a>
           : Industrial Design
         </p>
-        <p className="team-member">
+        <p>
           <a href="https://www.howes.studio/" target="_blank">
             Benjamin Howes
           </a>
           : Interior Design
         </p>
-        <p className="team-member">
+        <p>
           <a
             href="https://issuu.com/traceyweisman/docs/tw_selectedworks"
             target="_blank"
@@ -102,21 +114,13 @@ function App() {
       <br></br>
       <h3>Fabrication Team</h3>
       <div>
-        <p className="team-member">Daryl Shelton</p>
-        <p className="team-member">Emma Winick</p>
-        <p className="team-member">Joanne Wu</p>
+        <p>Daryl Shelton</p>
+        <p>Emma Winick</p>
+        <p>Joanne Wu</p>
       </div>
       <br></br>
       <p className="read-the-docs">
-        Knit + Code is a mixed reality installation blending digital and
-        physical experiences into an immersive spectacle. At its core is a
-        minimal 3D-knit textile dynamically lit with a range of slowly changing
-        lights. This physical form creates a canvas for a digital mixed reality
-        experience: Viewers scan a QR code below the sculpture to launch into
-        the digital space, where a custom algorithm generates multi-colored
-        fibrous models that extend beyond the confines of the physical
-        structure. Knit + Code blurs the boundaries of digital fabrication and
-        spatial design, inviting viewers to explore the space in new ways.
+        All rights reserved by the Design team and Pratt Institute
       </p>
     </div>
   );
